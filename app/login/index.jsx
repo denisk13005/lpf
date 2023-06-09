2aca6c818601e7208d708655db1d96e33e782f71
denisk13005 <kirmanndenis@gmail.com>
2023-06-09 21:24:26 +0200

    ajout de dossier et de fichier header footer et login

---
+++ app/login/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.module.scss'
+
+const login = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  );
+};
+
+export default login;
