5dc04593a4644b25173d30e2dcaedd8421691efe
denisk13005 <kirmanndenis@gmail.com>
2023-06-24 19:20:49 +0200

    création et stockage du token lors de la création d'un nouvel utilisateur

--- app/api/user/signUp/route.js
+++ app/api/user/signUp/route.js
@@ -1,5 +1,7 @@
 import prisma from '@/lib/prismaInstance';
 import  bcrypt  from 'bcrypt';
+const jwt = require("jsonwebtoken");
+
 
 
 
@@ -16,6 +18,7 @@ export async function POST(req, res) {
     }
     else {
       const hashPassword = await bcrypt.hash(userInfos.password,10)
+ 
 
       const newUser = await prisma.User.create(
       {data:  {
@@ -24,10 +27,25 @@ export async function POST(req, res) {
           password: hashPassword
         }}
       )
+      const  token = jwt.sign(
+        { userId: newUser.id }, //on signe le token avec l'id de l'utilisateur pour ne pas qu'un autre utilisateur puisse modifier un produit (seul celui avec l'id peut modifier ses produits)
+        process.env.JWT_SECRET, //on définit un sel pour le token
+        { expiresIn: "24h" } //on définit une durée de validité du token
+      )
+      console.log(token);
+      console.log(newUser);
+      const Account = await prisma.Account.create({
+        data: {
+          userId : newUser.id,
+          access_token : token
+        }
+      })
+      // console.log(Account);
       return new Response(
         JSON.stringify({
           status: 201,
           newUser,
+          Account
         })
       );
     }
