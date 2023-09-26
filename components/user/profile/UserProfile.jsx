b214ee1dd8011d09782323754a3b3e1f9f3ae3f2
denisk13005 <kirmanndenis@gmail.com>
2023-09-26 18:19:20 +0200

    page profil

--- components/user/profile/UserProfile.jsx
+++ components/user/profile/UserProfile.jsx
@@ -1,18 +1,32 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import { useUserContext } from '/context/UserContext';
 import { redirect } from 'next/navigation';
+import styles from './styles.module.scss'
+import Image from 'next/image';
 
 const UserProfile = () => {
   const { user } = useUserContext();
-  if(!user)
-{
- redirect('/login')
-}
+  const [changeDescription, setChangeDescription] = useState(false)
+  const [description, setDescription] = useState('')
+  if (!user) {
+    redirect('/login')
+  }
+
 
   return (
-    <div>
-      wELCOME {user.name}
+    <div className={styles.userProfileContainer}>
+      <h2>
+        Bonjour, {user.name}
+      </h2>
+      <div className={styles.pictureContainer}>
+        <Image src='/chien.jpg' fill alt="photo de profil de l'utilisateur " />
+      </div>
+      {
+        changeDescription ? <textarea name="" id="" cols="30" rows="10" onChange={e => setDescription(e.target.value)}></textarea> :
+          <div>{description}</div>
+      }
+      <button className={styles.btn} onClick={() => setChangeDescription(!changeDescription)}>{!changeDescription ? 'Ajouter une description' : 'enregistrer la description'}</button>
     </div>
   );
 };
